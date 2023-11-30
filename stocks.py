76d1ee3cc9e3949950bb2c0fffd89a97c1dc029a
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-11-30 14:55:15 +0300

    Implement logic for input panel and buttons

--- stocks.py
+++ stocks.py
@@ -7,6 +7,55 @@ class App(ctk.CTk):
         self.geometry("900x800")
         self.title("Stock Market")
 
+        # data
+        self.input_string = ctk.StringVar(value="AAPL")
+        self.time_string = ctk.StringVar(value=TIME_OPTIONS[0])
+
+        # widgets
+        InputPanel(self,self.input_string,self.time_string)
+
         self.mainloop()
+
+class InputPanel(ctk.CTkFrame):
+    def __init__(self,parent,input_string,time_string):
+        super().__init__(parent,fg_color=INPUT_BG_COLOR,corner_radius=0)
+        self.pack(fill="both",side="bottom")
+
+        # widgets
+        ctk.CTkEntry(self,
+                     textvariable=input_string,
+                     fg_color=BG_COLOR,
+                     border_color=TEXT_COLOR,
+                     border_width=1,
+                     text_color="white").pack(side="left",padx=10,pady=10)
+        
+        self.buttons = [TextButton(self,text,time_string) for text in TIME_OPTIONS]
+
+        time_string.trace("w",self.unselect_all_buttons)
+    
+    def unselect_all_buttons(self,*args):
+        for button in self.buttons:
+            button.unselect()
+
+class TextButton(ctk.CTkLabel):
+    def __init__(self,parent,text,time_string):
+        super().__init__(parent,text=text,text_color=TEXT_COLOR)
+        self.pack(side="right",padx=10,pady=10)
+
+        self.bind("<Button>",self.select_handler)
+
+        self.time_string = time_string
+        self.text = text
+
+        if time_string.get() == text:
+            self.select_handler()
+    
+    def select_handler(self,event=None):
+        self.time_string.set(self.text)
+        self.configure(text_color=HIGHLIGHT_COLOR)
+    
+    def unselect(self):
+        self.configure(text_color=TEXT_COLOR)
+
 
 App()
